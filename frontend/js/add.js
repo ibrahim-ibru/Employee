ac0a0fc440865758e607a630a8f08887f086ff0a
ibrahim-ibru <mohammedibrahim2k02@gmail.com>
2024-12-10 13:00:31 +0530

    updated

---
+++ frontend/js/add.js
@@ -0,0 +1,20 @@
+document.getElementById("addemployee-form").addEventListener("submit",(event)=>{
+    event.preventDefault()
+    const name=document.getElementById("first-name").value+" "+document.getElementById("last-name").value
+    const gender = document.querySelector('input[name="gender"]:checked')?.value;
+    const age=document.getElementById("age").value
+    const email =document.getElementById("email").value
+    const phone=document.getElementById("phone").value
+    const address=document.getElementById("address").value
+    const jobtype=document.getElementById("jobtype").value
+    const salary=document.getElementById("salary").value
+    const data={name,gender,age,email,phone,address,jobtype,salary}
+    console.log(data);
+    fetch("http://localhost:3000/submit",{
+        method: "POST"
+        ,headers:{"Content-Type":"text/json"},
+        body:JSON.stringify(data)
+    })
+
+
+})
